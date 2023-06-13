8345d9fa241b7fb672787da16dc50ab50d8a2d9f
BossBoxing <boss_slam@hotmail.com>
2023-06-14 02:53:39 +0700

    card of products

--- client/src/Components/Showcase.js
+++ client/src/Components/Showcase.js
@@ -1,37 +1,49 @@
 
 const data = [
     {
-        Id : 1,
-        Name : "Nike Air 1",
-        Price : 9900,
+        Id: 1,
+        Name: "Nike Air 1",
+        Price: 9900,
     },
     {
-        Id : 2,
-        Name : "Gucci Belt",
-        Price : 15000,
+        Id: 2,
+        Name: "Gucci Belt",
+        Price: 15000,
     },
     {
-        Id : 3,
-        Name : "Adidas NMD R2",
-        Price : 8000,
+        Id: 3,
+        Name: "Adidas NMD R2",
+        Price: 8000,
     },
     {
-        Id : 4,
-        Name : "Mc Yeans",
-        Price : 2200,
+        Id: 4,
+        Name: "Mc Yeans",
+        Price: 2200,
     },
 ];
 
-const ShowcaseDOM = data.map(a => {return (
-    <p>Id : {a.Id}</p>
-)})
+const ShowcaseDOM = data.map(a => {
+    return (
+        <>
+            <div className="card">
+                <img src="https://cdn-icons-png.flaticon.com/512/43/43777.png"
+                alt="product"
+                width={100}
+                className="flex center"
+                />
+                <p>{a.Name}</p>
+                <p>{a.Price}</p>
+            </div>
+        </>
+    );
+})
 
 export default function Showcase() {
-    <div className="container mx-auto">
-        <ul>
-            <li>
+    return (
+        <>
+            <div className="grid grid-cols-3 gap-2">
                 {ShowcaseDOM}
-            </li>
-        </ul>
-    </div>
+            </div >
+        </>
+    );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    }
   },
   plugins: [],
 }
