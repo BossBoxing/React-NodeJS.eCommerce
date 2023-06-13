dbec81a9a082bd914e57b7bda6cef99fabd5ddaa
BossBoxing <boss_slam@hotmail.com>
2023-06-13 22:49:35 +0700

    Add Navbar Component

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+
+
+export default function Navbar() {
+    return (
+        <nav className="Navbar">
+            <ul>
+                <li><a href="default.asp">Home</a></li>
+                <li><a href="news.asp">News</a></li>
+                <li><a href="contact.asp">Contact</a></li>
+                <li><a href="about.asp">About</a></li>
+            </ul>
+        </nav>
+    );
+}
