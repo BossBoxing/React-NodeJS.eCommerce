fe462f6c9e7641e510a76097ff1aaed1c2a9514a
BossBoxing <boss_slam@hotmail.com>
2023-06-15 20:31:49 +0700

    AddProduct Component

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 // Components
 import Navbar from './Components/Navbar.js';
 import Product from './Components/Products';
+import AddProduct from './Components/AddProduct';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <AddProduct/>
       <Product/>
     </div>
   );
---
+++ client/src/Components/AddProduct.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const AddProduct = () => {
+    return (
+        <>
+            <div className="h-6">
+                <h1>AddProduct</h1>
+                <div className="grid grid-cols-2 h-6">
+                    Item
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default AddProduct;
