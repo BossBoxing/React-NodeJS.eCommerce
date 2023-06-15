29ae86f59e45529b7f4f242bacafec01f1d19c18
BossBoxing <boss_slam@hotmail.com>
2023-06-15 20:54:47 +0700

    update AddProducts.js UI

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <AddProduct/>
-      <Product/>
+      {/* <Product/> */}
     </div>
   );
 }
--- client/src/Components/AddProduct.js
+++ client/src/Components/AddProduct.js
@@ -2,12 +2,57 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const AddProduct = () => {
+
+    function PostProduct()
+    {
+        axios.post('http://localhost:9000/products', () => {
+        
+        })
+    }
+
     return (
         <>
-            <div className="h-6">
-                <h1>AddProduct</h1>
-                <div className="grid grid-cols-2 h-6">
-                    Item
+            <div className="flex justify-center rounded bg-gray-400">
+                <div className="flex justify-center rounded bg-gray-400">
+                    <form>
+                        <h1>Add Product</h1>
+                        <br/>
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Product Name :
+                        </label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            autoComplete="name"
+                            className="rounded"
+                            placeholder="name" />
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Product Price :
+                        </label>
+                        <input
+                            type="text"
+                            name="price"
+                            id="price"
+                            className="rounded"
+                            autoComplete="price"
+                            placeholder="price" />
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Product Image Url :
+                        </label>
+                        <input
+                            type="text"
+                            name="image-url"
+                            id="image"
+                            className="rounded"
+                            autoComplete="image-url"
+                            placeholder="image-url" />
+                        <br/>
+                        <br/>
+                        <button className="bg-white rounded hover:bg-slate-800">
+                            Add Product
+                        </button>
+                    </form>
                 </div>
             </div>
         </>
